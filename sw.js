a9bf3284b1278aee0e1e0a6855750de866a22fd4
Togo800 <yoshizawa.togo@gmail.com>
2025-09-28 14:28:20 +0900

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,25 @@
+// sw.js
+const CACHE = 'athlog-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './manifest.webmanifest',
+  './favicon-16.png',
+  './favicon-32.png',
+  './apple-touch-icon-180.png',
+  './athlog-icon-192.png',
+  './athlog-icon-512.png'
+];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
